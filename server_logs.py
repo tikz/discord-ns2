51578490bc9aa02ff0ce8146ac8366ee97e6f90c
Tikzz <tik@lag.party>
2018-03-22 18:04:31 -0300

    Add logs sorting by date

--- server_logs.py
+++ server_logs.py
@@ -33,7 +33,7 @@ class Logs():
     def search(self, text):
         found = []
         pattern = re.compile(text, re.IGNORECASE)
-        log_files = sorted(os.listdir(self.log_dir), key=lambda x: x.split('-')[1]+x.split('-')[0], reverse=True)
+        log_files = sorted(os.listdir(self.log_dir), key=lambda x: x.split('-')[1]+x.split('-')[0])
         for log in log_files:
             log_path = os.path.join(self.log_dir, log)
             data = open(log_path, 'r', errors='replace')
